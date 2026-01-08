b32e1245ab81eb4c8be44bb1fb9b4115831c9f82
Zahartshenko-ship-it <Zahartshenko@icloud.com>
2026-01-08 16:46:08 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,16 @@
+class Apartment:
+    def __init__(self, address, rooms, total_area, living_area,
+                 floor, total_floors, owner, price):
+        self.address = address
+        self.rooms = int(rooms)
+        self.total_area = float(total_area)
+        self.living_area = float(living_area)
+        self.floor = int(floor)
+        self.total_floors = int(total_floors)
+        self.owner = owner
+        self.price = int(price)
+
+    def __repr__(self):
+        return (f"{self.address} | {self.rooms}к | "
+                f"этаж {self.floor}/{self.total_floors} | "
+                f"{self.price} руб.")
