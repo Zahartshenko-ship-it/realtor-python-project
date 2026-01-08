8261dfb9bd4f5129c24724b356d1bbe9a4a9402a
Zahartshenko-ship-it <Zahartshenko@icloud.com>
2026-01-08 16:46:32 +0500

    Add files via upload

---
+++ shell_sort.py
@@ -0,0 +1,15 @@
+# shell_sort.py
+
+def shell_sort(arr, key_func):
+    n = len(arr)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and key_func(arr[j - gap]) > key_func(temp):
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
