b5aa1ec6fd183213e0e7adcb1f8525941414b50e
Zahartshenko-ship-it <Zahartshenko@icloud.com>
2026-01-09 19:42:02 +0500

    data_saver.py

--- data_saver.py
+++ data_saver.py
@@ -1,11 +1,13 @@
-# data_saver.py
-
 def save_apartments(apartments, filename):
     """Сохраняет список квартир в CSV-файл (без кавычек, без запятых в адресе)"""
     with open(filename, 'w', encoding='utf-8') as f:
-        # Заголовок
         f.write("address,rooms,total_area,living_area,floor,total_floors,owner,price\n")
         for apt in apartments:
-            line = f"{apt.address},{apt.rooms},{apt.total_area},{apt.living_area}," \
-                   f"{apt.floor},{apt.total_floors},{apt.owner},{apt.price}\n"
+            line = (f"{apt.address},"
+                    f"{apt.rooms},"
+                    f"{apt.total_area},{apt.living_area},"
+                    f"{apt.floor},{apt.total_floors},"
+                    f"{apt.owner},"
+                    f"{apt.price}\n"
+            )
             f.write(line)
